f282a380918cfa9cc3bc01b36c2616039ea16d81
datfeela <datfeela@mail.ru>
2023-04-03 19:37:46 +0500

    Revert "add Breadcrumbs placeholder"
    This reverts commit 7d1f5999ea91e734928b98546ba82fca1b8d5bc6.

--- src/modules/features/Breadcrumbs/Breadcrumbs.tsx
+++
@@ -1,9 +0,0 @@
-import s from './Breadcrumbs.module.scss'
-
-export const Breadcrumbs = () => {
-    return (
-        <div className={s.wrap}>
-            Breadcrumbs {`->`} Breadcrumbs {`->`} Breadcrumbs {`->`} Breadcrumbs {`->`}
-        </div>
-    )
-}
