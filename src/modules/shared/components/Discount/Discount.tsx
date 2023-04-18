d3c66000e50166ad6824d5bcd557dc5a26e324b1
datfeela <datfeela@mail.ru>
2023-04-18 20:36:41 +0500

    Discount styling

--- src/modules/shared/components/Discount/Discount.tsx
+++ src/modules/shared/components/Discount/Discount.tsx
@@ -4,10 +4,19 @@ interface DiscountProps {
     price: number
     priceWithoutDiscount: number
     size?: 'md' | 'sm'
+    view?: 'square'
 }
 
-export const Discount = ({ price, priceWithoutDiscount, size }: DiscountProps) => {
-    const discount = Math.floor((1 - priceWithoutDiscount / price) * 100)
+export const Discount = ({ price, priceWithoutDiscount, size, view }: DiscountProps) => {
+    const discount = Math.floor((1 - price / priceWithoutDiscount) * 100)
 
-    return <div className={`${s.discount} ${size === 'sm' && s.discount_sm}`}>{discount}%</div>
+    return (
+        <div
+            className={`${s.discount} ${size === 'sm' ? s.discount_sm : ''} ${
+                view === 'square' ? s.discount_square : ''
+            }`}
+        >
+            -{discount}%
+        </div>
+    )
 }
