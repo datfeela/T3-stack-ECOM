f3f8d467fdb1d03ef925242335e9c92b9bfb1b4f
datfeela <datfeela@mail.ru>
2023-04-24 18:48:58 +0500

    replace Cart component with HeaderCart feature in Header

--- src/modules/widgets/Header/Header.tsx
+++ src/modules/widgets/Header/Header.tsx
@@ -1,19 +1,21 @@
+import { Cart } from '~/modules/features/HeaderCart'
 import { Auth } from './Components/Auth/Auth'
 import s from './Header.module.scss'
-import { Cart } from './Components/Cart/Cart'
 
 export const Header: React.FC = () => {
     return (
-        <header className={s.header}>
-            <div>nu tut menu</div>
-            <div>
-                <span className={s.logo}>nu tut logo</span>
-            </div>
-            <div className={s.right}>
-                <div>search feature</div>
-                <Auth />
-                <Cart />
-            </div>
-        </header>
+        <div className={s.wrap}>
+            <header className={s.header}>
+                <div>nu tut menu</div>
+                <div>
+                    <span className={s.logo}>nu tut logo</span>
+                </div>
+                <div className={s.right}>
+                    <div>search feature</div>
+                    <Auth />
+                    <Cart />
+                </div>
+            </header>
+        </div>
     )
 }
