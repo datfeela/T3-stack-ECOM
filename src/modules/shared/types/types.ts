35a690c4ad22a1f549d0d586ced58143985dbfeb
datfeela <datfeela@mail.ru>
2023-05-31 17:09:58 +0500

    add SignIn widget

--- src/modules/shared/types/types.ts
+++ src/modules/shared/types/types.ts
@@ -1,2 +1,9 @@
+import type { BuiltInProviderType } from 'next-auth/providers'
+import type { ClientSafeProvider, LiteralUnion } from 'next-auth/react'
+
 export type DefaultColor = 'yellow' | 'red' | 'blue' | 'purple'
 export type ImageOrientation = '16/9' | '16/10' | '4/5' | '21/9' | '3/4' | 'unset'
+export type AuthProviders = Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+> | null
