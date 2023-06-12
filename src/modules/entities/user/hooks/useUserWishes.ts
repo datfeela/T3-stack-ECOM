f77e18111340acfa3c78c1c0999a4b4344065d8c
datfeela <datfeela@mail.ru>
2023-06-12 13:36:29 +0500

    remove unused hook

--- src/modules/entities/user/hooks/useUserWishes.ts
+++
@@ -1,9 +0,0 @@
-import { useSession } from 'next-auth/react'
-import { api } from '~/modules/shared/api/apiTRPC'
-
-export const useUserWishes = () => {
-    const { data: sessionData } = useSession()
-    const userId = sessionData?.user.id
-
-    return api.user.getUserWishes.useQuery(userId || '')?.data?.wishedProducts
-}
