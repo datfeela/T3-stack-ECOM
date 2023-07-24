062a78b4e672750295f301b882e88b3c29e85b28
datfeela <datfeela@mail.ru>
2023-07-24 11:35:08 +0500

    update Divider: add variant w/ smaller margin

--- src/modules/shared/components/Divider/Divider.tsx
+++ src/modules/shared/components/Divider/Divider.tsx
@@ -1,5 +1,15 @@
 import s from './Divider.module.scss'
 
-export const Divider = ({ height = 2 }: { height?: number }) => {
-    return <div style={{ height: `${height}px` }} className={s.divider} />
+interface DividerProps {
+    height?: number
+    margin?: 'default' | 'sm'
+}
+
+export const Divider = ({ height = 2, margin = 'default' }: DividerProps) => {
+    return (
+        <div
+            style={{ height: `${height}px` }}
+            className={`${s.divider} ${margin === 'sm' ? s.divider_marginSm : ''}`}
+        />
+    )
 }
